ea8f2b07d925bfc792fb3aab60bc972daead6b8d
XddAdd <xddadd@qq.com>
2021-07-13 15:04:19 +0800

    'big_update'

--- src/test/java/com/add/Test.java
+++ src/test/java/com/add/Test.java
@@ -2,21 +2,66 @@ package com.add;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.add.domain.Clock;
+import com.add.domain.MorningClock;
+import com.add.domain.NoonClock;
+import com.add.domain.User;
 
 
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
+import java.util.List;
 
 
 public class Test {
 
     @org.junit.Test
     public void test1() throws UnsupportedEncodingException {
-        String s = RandomUtil.randomUUID();
-        String t = "68c8a272b458dd09b19972559c5bc13b";
-        System.out.println(t);
-        s = s.replace("-", "");
-        System.out.println(s);
+        Clock morningClock = new MorningClock();
+        Clock noonClock = new NoonClock();
+        Date now = DateUtil.date();
+        now.setHours(6);now.setMinutes(0);now.setSeconds(0);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(8);now.setMinutes(59);now.setSeconds(0);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(9);now.setMinutes(0);now.setSeconds(1);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(12);now.setMinutes(0);now.setSeconds(0);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(12);now.setMinutes(0);now.setSeconds(05);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(14);now.setMinutes(50);now.setSeconds(0);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+        now.setHours(15);now.setMinutes(1);now.setSeconds(1);
+        System.out.println(now);
+        System.out.println(morningClock.validTime(now));
+        System.out.println(noonClock.validTime(now));
+        System.out.println("---------------");
+    }
 
+    @org.junit.Test
+    public void test2() {
+        List<User> users = Main1.users;
+        Clock morningClock = new MorningClock();
+        Clock noonClock = new NoonClock();
+        morningClock.sendFailureEmail(users.get(0));
     }
 }
